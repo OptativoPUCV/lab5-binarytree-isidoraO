fb9e26cf78abfd60dd0c3c8fee801856599473b1
Your Name <you@example.com>
2024-05-10 22:27:52 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -186,7 +186,7 @@ Pair * nextTreeMap(TreeMap * tree)
     TreeNode *aux_parent = aux->parent;
     while(aux_parent->parent != NULL && tree->lower_than(aux_parent->pair->key, aux->pair->key))
         aux_parent = aux_parent->parent;  
-    aux = aux_parent;
+    //aux = aux_parent;
   }
   return aux->pair;
 }
