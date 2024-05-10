d5041e38741943cf0c9addfa886fb11d13060813
Your Name <you@example.com>
2024-05-10 23:08:54 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -181,7 +181,7 @@ Pair * upperBound(TreeMap * tree, void* key)
     tree->current = aux_upper;
     return aux_upper->pair;
   }
-  return NULL;
+  return aux_upper->pair;
 }
 
 Pair * firstTreeMap(TreeMap * tree) 
